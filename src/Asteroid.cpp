622bfa811f19651fc4c241789567b8d0e37fd7af
Omar Ben Brahim <om3rbb@gmail.com>
2026-01-14 00:27:26 +0100

    switched to namespace instead of enum for the AsteroidSize and did some corrections

--- src/Asteroid.cpp
+++ src/Asteroid.cpp
@@ -1,39 +1,25 @@
 #include "Asteroid.h"
 
-bool logged = false;
-
-Asteroid::Asteroid(AsteroidSize size_, Vector2 pos_) : pos(pos_), size(size_) {
-
+Asteroid::Asteroid(float size_, Vector2 pos_, Vector2 vel_)
+    : size(size_), pos(pos_), vel(vel_) {
   nb_of_sides = random_range<int>(MIN_NB_SIDES, MAX_NB_SIDES);
-
   asteroid_map = new Vector2[nb_of_sides];
-
   float ran_r = random_range<float>(MIN_R, MAX_R);
-
   float ran_ang = random_range<float>(0.7 * (ANGLE_DEG / nb_of_sides),
                                       ANGLE_DEG / nb_of_sides);
   float ran_ang_b = 0.0f;
-
   for (size_t i = 0; i < nb_of_sides; i++) {
     asteroid_map[i].x = ran_r * cos((ran_ang + ran_ang_b) * DEG2RAD);
     asteroid_map[i].y = ran_r * sin((ran_ang + ran_ang_b) * DEG2RAD);
-
     ran_r = random_range<float>(MIN_R, MAX_R);
-
     ran_ang_b += ran_ang;
     ran_ang = random_range<float>(0.7 * (ANGLE_DEG / nb_of_sides),
                                   ANGLE_DEG / nb_of_sides);
-
-    if (!logged) {
-      TraceLog(LOG_INFO, TextFormat("seed: %f | %d: {%f, %f}", ran_r, i,
-                                    asteroid_map[i].x, asteroid_map[i].y));
-    }
   }
-  logged = true;
 }
 
 Asteroid::~Asteroid() { delete[] asteroid_map; }
 
 void Asteroid::draw_asteroid() {
-  draw_lines(asteroid_map, nb_of_sides, pos, 100.0f, 0.0f, 1.0f);
+  draw_lines(asteroid_map, nb_of_sides, pos, size, 0.0f, 1.0f);
 }
--- src/Asteroid.h
+++ src/Asteroid.h
@@ -8,18 +8,24 @@
 #include <iostream>
 #include <raylib.h>
 
-typedef enum AsteroidSize { BIG, MEDIUM, SMALL } AsteroidSize;
+// typedef enum AsteroidSize { BIG = 30, MEDIUM = 15, SMALL = 5 } AsteroidSize;
+
+namespace AsteroidSize {
+constexpr float BIG = SCALE * 2.0f;
+constexpr float MEDIUM = SCALE * 1.4f;
+constexpr float SMALL = SCALE * 0.8;
+} // namespace AsteroidSize
 
 class Asteroid {
 private:
   Vector2 *asteroid_map;
-
   Vector2 pos;
-  AsteroidSize size;
+  Vector2 vel;
+  float size;
   int nb_of_sides;
 
 public:
-  Asteroid(AsteroidSize size_, Vector2 pos_);
+  Asteroid(float size_, Vector2 pos_, Vector2 vel);
   ~Asteroid();
 
   void draw_asteroid();
