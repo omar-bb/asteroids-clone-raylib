f75802da1aba9975e96dbcc8a0a517c7da92a55d
Omar Ben Brahim <om3rbb@gmail.com>
2026-01-16 01:50:05 +0100

    refactor the Asteroid class and added an enum class instead of a namespace to represent the asteroid's size
    make the intrinsic asteroid properties private instead of public and added getters and setters and switched to an std::array to represent the the asteroid map (may switch to an std::span)

--- src/Asteroid.cpp
+++ src/Asteroid.cpp
@@ -1,25 +1,41 @@
 #include "Asteroid.h"
+#include "raylib.h"
 
-Asteroid::Asteroid(float size_, Vector2 pos_, Vector2 vel_)
-    : size(size_), pos(pos_), vel(vel_) {
-  nb_of_sides = random_range<int>(MIN_NB_SIDES, MAX_NB_SIDES);
-  asteroid_map = new Vector2[nb_of_sides];
+Asteroid::Asteroid(float size, Vector2 pos, Vector2 vel)
+    : size_(size), pos_(pos), vel_(vel) {
+  nb_of_sides_ = random_range<int>(MIN_NB_SIDES, MAX_NB_SIDES);
+
   float ran_r = random_range<float>(MIN_R, MAX_R);
-  float ran_ang = random_range<float>(0.7 * (ANGLE_DEG / nb_of_sides),
-                                      ANGLE_DEG / nb_of_sides);
+  float ran_ang = random_range<float>(0.7 * (ANGLE_DEG / nb_of_sides_),
+                                      ANGLE_DEG / nb_of_sides_);
   float ran_ang_b = 0.0f;
-  for (size_t i = 0; i < nb_of_sides; i++) {
+  for (size_t i = 0; i < nb_of_sides_; i++) {
     asteroid_map[i].x = ran_r * cos((ran_ang + ran_ang_b) * DEG2RAD);
     asteroid_map[i].y = ran_r * sin((ran_ang + ran_ang_b) * DEG2RAD);
     ran_r = random_range<float>(MIN_R, MAX_R);
     ran_ang_b += ran_ang;
-    ran_ang = random_range<float>(0.7 * (ANGLE_DEG / nb_of_sides),
-                                  ANGLE_DEG / nb_of_sides);
+    ran_ang = random_range<float>(0.7 * (ANGLE_DEG / nb_of_sides_),
+                                  ANGLE_DEG / nb_of_sides_);
   }
 }
 
-Asteroid::~Asteroid() { delete[] asteroid_map; }
+// getters
+Vector2 Asteroid::get_pos() const { return pos_; }
+Vector2 Asteroid::get_vel() const { return vel_; }
+AsteroidSize Asteroid::get_size() const {
+  return static_cast<AsteroidSize>(size_);
+}
+
+// setters
+void Asteroid::set_pos(const Vector2 &pos) {
+  pos_.x = pos.x;
+  pos_.y = pos.y;
+}
+void Asteroid::set_vel(const Vector2 &vel) {
+  vel_.x = vel.x;
+  vel_.y = vel.y;
+}
 
 void Asteroid::draw_asteroid() {
-  draw_lines(asteroid_map, nb_of_sides, pos, size, 0.0f, 1.0f);
+  draw_lines(asteroid_map, pos_, size_, 0.0f, 1.0f, nb_of_sides_);
 }
--- src/Asteroid.h
+++ src/Asteroid.h
@@ -8,27 +8,49 @@
 #include <iostream>
 #include <raylib.h>
 
-// typedef enum AsteroidSize { BIG = 30, MEDIUM = 15, SMALL = 5 } AsteroidSize;
+enum class AsteroidSize { BIG, MEDIUM, SMALL };
 
-namespace AsteroidSize {
-constexpr float BIG = SCALE * 2.0f;
-constexpr float MEDIUM = SCALE * 1.4f;
-constexpr float SMALL = SCALE * 0.8;
-} // namespace AsteroidSize
+class AsteroidSizeWrapper {
+public:
+  AsteroidSizeWrapper(AsteroidSize size) : size_(size) {};
+
+  float size() {
+    constexpr float size[] = {ASTER_BIG, ASTER_MEDIUM, ASTER_SMALL};
+    return size[static_cast<int>(this->size_)];
+  }
+
+  float vel_scale() {
+    const float vel_scale[] = {ASTER_VELSCALE_BIG, ASTER_VELSCALE_MEDIUM,
+                               ASTER_VELSCALE_SMALL};
+    return vel_scale[static_cast<int>(this->size_)];
+  }
+
+private:
+  AsteroidSize size_;
+};
 
 class Asteroid {
-private:
-  Vector2 *asteroid_map;
-  Vector2 pos;
-  Vector2 vel;
-  float size;
-  int nb_of_sides;
-
 public:
   Asteroid(float size_, Vector2 pos_, Vector2 vel);
-  ~Asteroid();
+
+  ~Asteroid() = default;
+
+  Vector2 get_pos() const;
+  Vector2 get_vel() const;
+  AsteroidSize get_size() const;
+
+  void set_pos(const Vector2 &pos);
+  void set_vel(const Vector2 &vel);
 
   void draw_asteroid();
+
+private:
+  std::array<Vector2, MAX_NB_SIDES> asteroid_map;
+  int nb_of_sides_;
+
+  Vector2 vel_;
+  Vector2 pos_;
+  float size_;
 };
 
 #endif
