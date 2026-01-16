77350d6d541a0b024ddf1ca2423691ebc01d2338
Omar Ben Brahim <om3rbb@gmail.com>
2026-01-16 01:53:53 +0100

    refactor the Spaceship class
    made the intrinsic spaceship properties private and added getters and setters to access them and change their values.
    
    update the reference to the draw_line function so that it gets the appropriate parameters

--- src/Spaceship.cpp
+++ src/Spaceship.cpp
@@ -1,21 +1,43 @@
 #include "Spaceship.h"
+#include "constants.h"
+#include "raylib.h"
+
+const std::array<Vector2, SHIP_MAP_LENGTH> Spaceship::ship_map = {SHIP_MAP};
+const std::array<Vector2, THRUST_MAP_LENGTH> Spaceship::thrust_map = {
+    THRUST_MAP};
 
 Spaceship::Spaceship() {
-  pos.x = (float)SCREEN_WIDTH / 2;
-  pos.y = (float)SCREEN_HEIGHT / 2;
-
-  rot = DEG2RAD * 180.0f;
-
-  ship_dir.x = cos(rot);
-  ship_dir.y = sin(rot);
+  pos_ = {SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f};
+  vel_ = {0.0f, 0.0F};
+  rot_ = DEG2RAD_(180.0f);
+  ship_dir_ = {cos(rot_), sin(rot_)};
 }
 
-void Spaceship::draw_ship() { // draw_lines(ship_map, 5, position, 16.0f, 1.0f);
-  draw_lines(ship_map, sizeof(ship_map) / sizeof(ship_map[0]), pos, SCALE, rot,
-             THICKNESS);
+// getters
+Vector2 Spaceship::get_pos() const { return pos_; }
+Vector2 Spaceship::get_vel() const { return vel_; }
+float Spaceship::get_rot() const { return rot_; }
+Vector2 Spaceship::get_ship_dir() const { return ship_dir_; }
+
+// setters
+void Spaceship::set_pos(const Vector2 &pos) {
+  pos_.x = pos.x;
+  pos_.y = pos.y;
+}
+void Spaceship::set_vel(const Vector2 &vel) {
+  vel_.x = vel.x;
+  vel_.y = vel.y;
+}
+void Spaceship::set_rot(float rot) { rot_ = rot; }
+void Spaceship::set_ship_dir(const Vector2 &ship_dir) {
+  ship_dir_.x = ship_dir.x;
+  ship_dir_.y = ship_dir.y;
+}
+
+void Spaceship::draw_ship() {
+  draw_lines(ship_map, pos_, SCALE, rot_, THICKNESS);
 }
 
 void Spaceship::draw_thrust() {
-  draw_lines(thrust_map, sizeof(thrust_map) / sizeof(thrust_map[0]), pos, SCALE,
-             rot, THICKNESS);
+  draw_lines(thrust_map, pos_, SCALE, rot_, THICKNESS);
 }
--- src/Spaceship.h
+++ src/Spaceship.h
@@ -7,25 +7,36 @@
 #include <raylib.h>
 
 class Spaceship {
-private:
 public:
-  static constexpr Vector2 ship_map[] = SHIP_MAP;
-  static constexpr Vector2 thrust_map[] = THRUST_MAP;
+  static const std::array<Vector2, SHIP_MAP_LENGTH> ship_map;
+  static const std::array<Vector2, THRUST_MAP_LENGTH> thrust_map;
 
-  Vector2 pos;
-  Vector2 vel;
-  float rot;
-  Vector2 ship_dir;
-
+  // constructor
   Spaceship();
-  // void set_rot(float rot_) { rot = rot_; };
 
-  // update
-  void handle_movement();
+  // destructor
+  ~Spaceship() = default;
+
+  // setters and getters
+  Vector2 get_pos() const;
+  Vector2 get_vel() const;
+  float get_rot() const;
+  Vector2 get_ship_dir() const;
+
+  void set_pos(const Vector2 &pos);
+  void set_vel(const Vector2 &vel);
+  void set_rot(float rot);
+  void set_ship_dir(const Vector2 &ship_dir);
 
   // render
   void draw_ship();
   void draw_thrust();
+
+private:
+  Vector2 pos_;
+  Vector2 vel_;
+  float rot_;
+  Vector2 ship_dir_;
 };
 
 #endif
